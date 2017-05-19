983dfe1cafc0b7a4da43bea5bec668beb666e78b
Tony Duckles <tony@nynim.org>
2017-05-19 17:40:09 -0500

    .bash_completion.d/misc.bash: Remove git-track completion

--- .bash_completion.d/misc.bash
+++ .bash_completion.d/misc.bash
@@ -5,6 +5,3 @@ complete -F _known_hosts whois nslookup nmap
 complete -F _known_hosts push_ssh_cert
 
 complete -o nospace -A command killall
-
-# git-track completes remote names
-complete -o default -o nospace -F _git_checkout git-track
